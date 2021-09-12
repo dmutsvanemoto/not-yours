587783d07607cf38e827978e20d122c2dc005726
dmutsvanemoto <newkuda@gmail.com>
2021-09-12 12:44:11 +0100

    feat(chat): created chat app with default nx template feat(chat): created chat app e2e with default nx template

---
+++ apps/not-your-chat/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'not-your-chat',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/not-your-chat',
+};
